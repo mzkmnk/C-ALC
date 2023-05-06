295ee3a71ddf07541ff37c9ebb46bb55df0f03de
mizuki <114119396+mzkmnk@users.noreply.github.com>
2023-05-06 21:24:13 +0900

    Update c-snipet.cpp

--- c-snipet.cpp
+++ c-snipet.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 #include <atcoder/all>
 using namespace std;
+using namespace atcoder;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 ll MOD = pow(10,9)+7;
 ll INF = 1e18;
+
+priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> q;
 
 int main() {
 }
