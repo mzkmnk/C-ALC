e7039f6098cc529d48eae86eb22292a03d4fb4c3
mizuki <114119396+mzkmnk@users.noreply.github.com>
2023-05-06 21:51:20 +0900

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,6 +1,5 @@
-//vector<vector<pair<ll, ll>>> g(n)でg[n].push_back({t,d})とできる。
-//dijkstra(ノード数、隣接リスト、始点ノード)
-vector<ll> dijkstra(ll n, vector<vector<pair<ll, ll>>> g, ll s) {
+// dijkstra(ノード数、隣接リスト、始点ノード)
+vector<ll> dijkstra(ll n, unordered_map<ll, vector<pair<ll, ll>>> g, ll s) {
     priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> q; // 優先度付きキュー
     vector<ll> dist(n, INF); // 始点からの最短距離
     dist[s] = 0; // 始点から始点への距離は0
