f8e265d59de752fbca825e99fdadcd855951e52a
mizuki <114119396+mzkmnk@users.noreply.github.com>
2023-05-06 21:21:54 +0900

    Create dijkstra.cpp

---
+++ dijkstra.cpp
@@ -0,0 +1,23 @@
+//vector<vector<pair<ll, ll>>> g(n)でg[n].push_back({t,d})とできる。
+//dijkstra(ノード数、隣接リスト、始点ノード)
+vector<ll> dijkstra(ll n, vector<vector<pair<ll, ll>>> g, ll s) {
+    priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> q; // 優先度付きキュー
+    vector<ll> dist(n, INF); // 始点からの最短距離
+    dist[s] = 0; // 始点から始点への距離は0
+    q.push(make_pair(dist[s], s));
+
+    while (!q.empty()) {
+        pair<ll, ll> p = q.top();
+        q.pop();
+        ll v = p.second;
+        if (dist[v] < p.first) continue;
+        for (auto e : g[v]) {
+            if (dist[e.first] > dist[v] + e.second) {
+                dist[e.first] = dist[v] + e.second;
+                q.push(make_pair(dist[e.first], e.first));
+            }
+        }
+    }
+
+    return dist;
+}
