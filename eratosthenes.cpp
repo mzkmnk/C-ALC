fbac5c9f3ca65b3ac64e59ad4464fa5e1b291b04
mizuki <114119396+mzkmnk@users.noreply.github.com>
2023-05-09 11:55:59 +0900

    Update eratosthenes.cpp

--- eratosthenes.cpp
+++ eratosthenes.cpp
@@ -1,10 +1,10 @@
-vector<int> sieve_of_eratosthenes(int N) {
+vector<ll> sieve(ll N) {
     vector<bool> num(N+1,false);num[0] = true,num[1] = true;
-    vector<int> primes;
-    for(int i = 2; i < N+1; ++i){
+    vector<ll> primes;
+    for(ll i = 2; i < N+1; ++i){
         if(num[i]) continue;
         primes.push_back(i);
-        for(int j = 2; i*j <= N; ++j) num[i*j] = true;
+        for(ll j = 2; i*j <= N; ++j) num[i*j] = true;
     }
     return primes;
 }
